1319c6db2dc137444f8623fbe36a6ce584dec872
SaravanaKumar <gsaravanakumar1101@gmail.com>
2024-10-10 11:21:42 +0530

    Ui-v1

--- src/compounds/HomePage/HomePage.js
+++ src/compounds/HomePage/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../HomePage/HomePage.css';
 import { useNavigate } from 'react-router-dom';
-import image1 from '../../assets/image.png';
-import image2 from '../../assets/image-2.avif';
-import image3 from '../../assets/image-3.avif';
-import image4 from '../../assets/image-4.jpg';
+// import image1 from '../../assets/image.png';
+// import image2 from '../../assets/image-2.avif';
+// import image3 from '../../assets/image-3.avif';
+// import image4 from '../../assets/image-4.jpg';
 
 
 const HomePage = () => {
@@ -22,12 +22,12 @@ const HomePage = () => {
         <div className="home-page">
             <div className="home-btm">
                 <div className="container">
-                    <div className="image-gallery">
+                    {/* <div className="image-gallery">
                         <img src={image1} alt="image-1" />
                         <img src={image2} alt="image-2" />
                         <img src={image3} alt="image-3" />
                         <img src={image4} alt="image-4" />
-                    </div>
+                    </div> */}
 
                     <div className="home-btm-2">
                         <div className="home-btm-cnt">
